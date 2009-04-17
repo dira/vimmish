7a05b2bee3a605e125273f2f313730859023445d
Irina Dumitrascu <irina2000d@yahoo.com>
2009-04-17 22:05:47 +0200

    test: don't parse commands that don't end in normal mode

--- test/smoke_test.rb
+++ test/smoke_test.rb
@@ -3,9 +3,6 @@ parser = VimParser.new
 
 describe "smoke tests" do
   ["d", "y"].each do |command|
-    it "should parse #{command}" do
-      parser.parse(command).should.not.be nil
-    end
     ["w", "("].each do |motion|
       it "should parse #{command}#{motion}" do
         parser.parse(command + motion).should.not.be nil
