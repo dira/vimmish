b1460fe56b7b92e5a5ac41d6f1681ffbf499ebe8
Irina Dumitrascu <irina2000d@yahoo.com>
2009-04-18 00:16:18 +0200

    cooler example

--- example.rb
+++ example.rb
@@ -1,4 +1,4 @@
 require 'lib/vimmish'
 
-vimtalk = 'iabcde fghij klmn 123<ESC>^2wBc2wbetter now<ESC><RIGHT>Da.<ESC>II can underst<ESC><RIGHT><RIGHT>c<RIGHT>n<ESC>' 
+vimtalk = "iDon't know vim :(.<ESC>^2WBc2wbetter now<ESC><RIGHT>Da.<ESC>II can understan<ESC><RIGHT>~dE"
 p Vimmish.humanize(vimtalk)
