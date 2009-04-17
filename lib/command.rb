4b9f6ed152a320a20e4e07bdefe96cd124594b0f
Irina Dumitrascu <irina2000d@yahoo.com>
2009-04-17 21:49:57 +0200

    refactored command

--- lib/command.rb
+++ lib/command.rb
@@ -1,28 +1,38 @@
 module Command
   def eval (mode = nil)
-    case text_value
-      when 'c' then 'change'
-      when 'd' then 'delete'
-      when 'y' then 'yank'
-      
-      when 'x' then 'delete a character'
-      when 'dd' then 'delete current line'
-      when 'D' then 'delete the rest of the current line'
-      when 'J' then 'unite the current line with the next one'
-      when '~' then 'change character case'
-      when 'gv' then 're-select previous selection'
-      
-      when 'cc' then 'change current line with: '
-      when 's' then 'substitute character with: '
-      when 'S' then 'substitute line with: '
-      when 'C' then 'change the rest of the current line with: ' 
-      
-      when 'i' then 'insert before cursor: '
-      when 'I' then 'insert to the begining of the current line: '
-      when 'a' then 'append after cursor: '
-      when 'A' then 'append to the end of the current line: '
-      when 'o' then 'open a new line below and insert: '
-      when 'O' then 'open a new line above and insert: '
-    end + (mode == 'selection' ? ' selection' : '')
+    translations = {
+      'c' => 'change',
+      'd' => 'delete',
+      'y' => 'yank',
+
+      'x' => 'delete a character',
+      'dd' => 'delete current line',
+      'D' => 'delete the rest of the current line',
+      'J' => 'unite the current line with the next one',
+      '~' => {:default => 'change character case', :selection => false},
+      'gv' => 're-select previous selection',
+
+      'cc' => 'change current line with: ',
+      's' => 'substitute character with: ',
+      'S' => 'substitute line with: ',
+      'C' => 'change the rest of the current line with: ' ,
+
+      'i' => 'insert before cursor: ',
+      'I' => 'insert to the begining of the current line: ',
+      'a' => 'append after cursor: ',
+      'A' => 'append to the end of the current line: ',
+      'o' => 'open a new line below and insert: ',
+      'O' => 'open a new line above and insert: ',
+    }
+    result = translations[text_value]
+    if (result.respond_to? :swapcase)
+      result + (mode == 'selection' ? ' selection' : '')
+    else
+      if (!mode.nil? && result[mode])
+        result[mode]
+      else
+        result[:default]
+      end
+    end
   end
 end
